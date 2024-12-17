0c0399c08acf5e03e713839989bd69b29d543941
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-17 12:14:55 -0500

    Update santajoker.lua

--- cards/santajoker.lua
+++ cards/santajoker.lua
@@ -1,1 +1,26 @@
-
+SMODS.Joker {
+    key = "santajoker",
+    loc_txt = {
+        name = "Santa",
+        text = {},
+    },
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
+    config = ,
+    rarity = 1,
+    atlas = "Orchid",
+    pos = { x = 0, y = 0 },
+    cost = 6,
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+            }
+        }
+    end,
+    calculate = function(self, card, context)
+       
+    end
+}
