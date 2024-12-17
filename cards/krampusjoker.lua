f22ccd8a8bb655ece158c6d1bab11265cd7ec954
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-17 12:32:32 -0500

    Adds krampusjoker.lua

---
+++ cards/krampusjoker.lua
@@ -0,0 +1,25 @@
+SMODS.Joker {
+    key = "krampusjoker",
+    loc_txt = {
+        name = "Krampus",
+        text = {},
+    },
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
+    config = { },
+    rarity = 1,
+    atlas = "Orchid",
+    pos = { x = 1, y = 0 },
+    cost = 6,
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = { }
+        }
+    end,
+    calculate = function(self, card, context)
+       
+    end
+}
