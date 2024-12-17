55cecdc6f44df198f2657585a400bc4d91f3ecb2
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-17 12:37:28 -0500

    Adds milkandcookies.lua

---
+++ cards/milkandcookies.lua
@@ -0,0 +1,25 @@
+SMODS.Joker {
+    key = "milkandcookies",
+    loc_txt = {
+        name = "Milk and Cookies",
+        text = {},
+    },
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
+    config = { },
+    rarity = 1,
+    atlas = "Orchid",
+    pos = { x = 2, y = 0 },
+    cost = 6,
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = { }
+        }
+    end,
+    calculate = function(self, card, context)
+       
+    end
+}
