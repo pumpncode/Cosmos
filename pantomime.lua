98ae4c85d3b7227dd65bc62cb7d3e9393dcae559
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-17 12:39:15 -0500

    Adds pantomime.lua

---
+++ pantomime.lua
@@ -0,0 +1,25 @@
+SMODS.Joker {
+    key = "pantomime",
+    loc_txt = {
+        name = "Pantomime",
+        text = {},
+    },
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
+    config = { },
+    rarity = 1,
+    atlas = "Orchid",
+    pos = { x = 3, y = 0 },
+    cost = 6,
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = { }
+        }
+    end,
+    calculate = function(self, card, context)
+       
+    end
+}
