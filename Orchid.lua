6f703e04fc3de454c9af5f2c93a3b30e52ee7283
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-17 11:50:59 -0500

    Added Orchid.lua

---
+++ Orchid.lua
@@ -0,0 +1,16 @@
+SMODS.Atlas {
+    key = "Orchid",
+    path = "Orchid.png",
+    px = 71,
+    py = 95,
+}
+
+local subdir = "cards"
+local cards = NFS.getDirectoryItems(SMODS.current_mod.path .. subdir)
+for _, filename in pairs(cards) do
+    local file, exception = SMODS.load_file(subdir .. "/" .. filename)
+    if exception then
+        error(exception)
+    end
+    file()
+end
