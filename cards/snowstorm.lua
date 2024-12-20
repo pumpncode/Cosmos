06b2c5c98fc4425dbd194660ca6ea43b2589a407
neato <163772215+neatoqueen@users.noreply.github.com>
2024-12-20 11:51:37 -0500

    Update snowstorm.lua

--- cards/snowstorm.lua
+++ cards/snowstorm.lua
@@ -11,7 +11,7 @@ SMODS.Joker {
     perishable_compat = true,
     config = { },
     rarity = 1,
-    atlas = "Orchid",
+    atlas = "Cosmos",
     pos = { x = 5, y = 0 },
     cost = 6,
     loc_vars = function(self, info_queue, card)
