364e4c45f471e6dba073e3044e3e77063486aecb
VictinZero <79382302+VictinZero@users.noreply.github.com>
2024-12-26 21:20:38 +0100

    Update effect of spinagogue.lua

--- cards/spinagogue.lua
+++ cards/spinagogue.lua
@@ -8,12 +8,14 @@ SMODS.Joker {
     loc_txt = {
         name = "Spinagogue Champion",
         text = {
-            "{C:green}#1# in #2#{} chance to remove",
-            "{C:dark_edition}Editions{} from {C:attention}scored cards",
-            "{s:0.33} ",
-            "{C:green}#3# in #4#{} chance to add {C:dark_edition}Foil,",
-            "{C:dark_edition}Holographic{}, or {C:dark_edition}Polychrome",
-            "to {C:attention}scored cards",
+            -- "{C:green}#1# in #2#{} chance to remove",
+            -- "{C:dark_edition}Editions{} from {C:attention}scored cards",
+            -- "{s:0.33} ",
+            "When {C:attention}scoring{} a card",
+            "in the {C:attention}first hand{} of round,",
+            "{C:green}#1# in #2#{} chance to add {C:dark_edition}Foil{},",
+            "{C:dark_edition}Holographic{}, or {C:dark_edition}Polychrome{} to it",
+            -- "to {C:attention}scored cards",
             -- "{C:green}#3# in #4#{} chance for base {C:attention}scored cards",
             -- "to become {C:dark_edition}Foil, {C:dark_edition}Holographic{}, or {C:dark_edition}Polychrome",
         },
@@ -23,42 +25,19 @@ SMODS.Joker {
     blueprint_compat = true,
     eternal_compat = false,
     perishable_compat = true,
-    config = { extra = { remove_odds = 4, add_odds = 4 } },
+    config = { extra = { add_odds = 4 } },
     rarity = 1,
     atlas = "JJPack",
     pos = { x = 6, y = 0 },
     cost = 6,
     loc_vars = function(self, info_queue, card)
         return {
-            vars = {G.GAME and G.GAME.probabilities.normal or 1, card.ability.extra.remove_odds, G.GAME and G.GAME.probabilities.normal or 1, card.ability.extra.add_odds}
+            vars = {G.GAME and G.GAME.probabilities.normal or 1, card.ability.extra.add_odds}
         }
     end,
     calculate = function(self, card, context)
-        if context.before and context.full_hand then
+        if context.before and context.cardarea == G.jokers and context.full_hand and G.GAME.current_round.hands_played == 0 then
             for i, v in ipairs(context.scoring_hand) do
-                if v.edition then
-                    if pseudorandom('jj_spinagogue_remove') < G.GAME.probabilities.normal / card.ability.extra.remove_odds then
-                        -- G.E_MANAGER:add_event(Event({
-                        --     trigger = 'immediate',
-                        --     delay = 0.0,
-                        --     blockable = true,
-                        --     blocking = true,
-                        --     func = function()
-                        v:set_edition(nil, true, false)
-                        --        return true
-                        --     end
-                        --   }))
-                        card_eval_status_text(v, 'extra', nil, nil, nil, {
-                            message = 'SHIN!',
-                            colour = G.C.GREEN
-                        })
-                    else
-                        card_eval_status_text(v, 'extra', nil, nil, nil, {
-                            message = 'NUN!',
-                            colour = G.C.GREEN
-                        })
-                    end
-                end
                 if not v.edition then
                     if pseudorandom('jj_spinagogue_add') < G.GAME.probabilities.normal / card.ability.extra.add_odds then
                         local edition = poll_edition('jj_spinagogue_edition', nil, true, true, { "e_foil", "e_holo", "e_polychrome" })
@@ -77,12 +56,22 @@ SMODS.Joker {
                                 message = 'GIMEL!',
                                 colour = G.C.GREEN
                             })
-                        else
+                        elseif edition == 'e_holo' then
                             card_eval_status_text(v, 'extra', nil, nil, nil, {
                                 message = "HEY!",
-                                colour = G.C.GREEN
+                                colour = G.C.RED
+                            })
+                        else
+                            card_eval_status_text(v, 'extra', nil, nil, nil, {
+                                message = "SHIN!",
+                                colour = G.C.BLUE
                             })
                         end
+                    else
+                        card_eval_status_text(v, 'extra', nil, nil, nil, {
+                            message = 'NUN!',
+                            colour = G.C.PURPLE
+                        })
                     end
                 end
             end
