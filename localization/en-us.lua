ef95b338a02eeb2c58e2547586bb5666fb8159f2
someone23832 <derpysquidtrollface@gmail.com>
2024-12-27 11:34:10 -0600

    Updated and fixed snowman
    Fixed descriptions, fixed scoring steps and status text order of snowman

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -6,7 +6,7 @@ return {
                 text = {
                     "Gains {C:chips}+#2#{} Chips per card scored",
                     "After Joker reaches {C:chips}#4#{} Chips:",
-                    "Lose {C:chips}-#3#{} Chips per card scored",
+                    "{C:chips}-#3#{} Chips per card scored",
                     "{C:inactive}(Currently {C:chips}#1#{C:inactive} Chips)"
                 }
             },
@@ -14,7 +14,7 @@ return {
                 name = 'Snowman',
                 text = {
 --                    "{C:inactive,s:0.7}The days are getting warmer...",
-                    "Loses {C:chips}-#3# Chips per card scored",
+                    "{C:chips}-#3#{} Chips per card scored",
                     "{C:inactive}(Currently {C:chips}#1#{C:inactive} Chips)"
                 }
             }
