3ce86b74bde13998abeeab7831b81f22999e8b85
Amy Hurrell <taliajhurrell@gmail.com>
2025-04-11 00:15:03 +0100

    Piano Roll (#20)
    Piano Roll
    
    Gains X0.5 Mult for
    every [rank] destroyed,
    rank changes after the [rank]s are destroyed
    (Currently X1 Mult)
    
    Co-authored-by: VictinZero <79382302+VictinZero@users.noreply.github.com>

--- packs/Music/pianoroll.lua
+++ packs/Music/pianoroll.lua
@@ -1,3 +1,21 @@
+function cosmos_reset_piano_rank()
+    G.GAME.current_round.cosmos_piano_card.rank = 'Ace'
+    local valid_cosmos_piano_cards = {}
+    for k, v in ipairs(G.playing_cards) do
+        if v.ability.effect ~= 'Stone Card' then
+            if not SMODS.has_no_rank(v) then
+                valid_cosmos_piano_cards[#valid_cosmos_piano_cards + 1] = v
+            end
+        end
+    end
+    if valid_cosmos_piano_cards[1] then
+        local cosmos_piano_card = pseudorandom_element(valid_cosmos_piano_cards,
+            pseudoseed('piano' .. G.GAME.round_resets.ante))
+        G.GAME.current_round.cosmos_piano_card.rank = cosmos_piano_card.base.value
+        G.GAME.current_round.cosmos_piano_card.id = cosmos_piano_card.base.id
+    end
+end
+
 SMODS.Joker {
     key = "pianoroll",
     unlocked = true,
@@ -7,18 +25,59 @@ SMODS.Joker {
     perishable_compat = true,
     rarity = 1,
     atlas = "MusicAtlas",
-    pos = { x = 1, y = 1 },
+    pos = {
+        x = 1,
+        y = 1
+    },
     cost = 5,
-    config = { extra = { x_mult = 1, x_mult_mod = 0.5} },
+    config = {
+        extra = {
+            x_mult = 1,
+            x_mult_mod = 0.5
+        }
+    },
+
     in_pool = function(self, args)
         local check = G.cosmos.enabled.Music or false
         return check
     end,
+
     loc_vars = function(self, info_queue, card)
         return {
-            vars = { card.ability.extra.x_mult_mod, localize(G.GAME.current_round.mail_card.rank, 'ranks'), card.ability.extra.x_mult }
+            vars = {card.ability.extra.x_mult_mod, localize(G.GAME.current_round.cosmos_piano_card.rank, 'ranks'),
+                    card.ability.extra.x_mult}
         }
     end,
+
     calculate = function(self, card, context)
+        if context.joker_main and card.ability.extra.x_mult > 1 then
+            return {
+                message = localize {
+                    type = 'variable',
+                    key = 'a_xmult',
+                    vars = {card.ability.extra.x_mult}
+                },
+                Xmult_mod = card.ability.extra.x_mult,
+                colour = G.C.MULT
+            }
+        elseif context.remove_playing_cards and not context.blueprint then
+            local targets = 0
+            for k, val in ipairs(context.removed) do
+                if val:get_id() == G.GAME.current_round.cosmos_piano_card.id then
+                    targets = targets + 1
+                end
+            end
+            if targets > 0 then
+                card.ability.extra.x_mult = card.ability.extra.x_mult + targets * card.ability.extra.x_mult_mod
+                cosmos_reset_piano_rank()
+                return {
+                    message = localize {
+                        type = 'variable',
+                        key = 'a_xmult',
+                        vars = {card.ability.extra.x_mult}
+                    }
+                }
+            end
+        end
     end
 }
